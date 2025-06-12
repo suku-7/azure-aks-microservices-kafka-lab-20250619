5e453f577232fa799d77c464eb99aca9db77e9e1
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2025-06-12 10:31:29 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,17 +6,7 @@ pip install httpie
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "../awscliv2.zip"
-#unzip -o ../awscliv2.zip -d ../
-#sudo .././aws/install
-
-#curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-#sudo mv /tmp/eksctl /usr/local/bin
-
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash 
-
-# If you are using GCP, Uncomment # to install gcloud tool.
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt-get update && sudo apt-get install google-cloud-cli
-sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ jammy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt update
+sudo apt install azure-cli
